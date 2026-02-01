8a2597c303e8f40c012c73bb1bf11c5242c2264e
Angel El Moucary <elmoucary.a@northeastern.edu>
2026-02-01 15:45:35 -0500

    conenction to db

--- backend/db/connection.py
+++ backend/db/connection.py
@@ -1,6 +1,14 @@
+import os
+from psycopg2 import connect
+from psycopg2.extensions import connection as Connection
+from dotenv import load_dotenv
 
-#I don't really know how to do this, so placeholder 
-def get_db():
-    pass 
+load_dotenv()
 
-
+def get_db() -> Connection:
+    """Get database connection"""
+    try:
+        conn = connect(os.getenv("postgresql://postgres.acwlniaolnhyqspatwaj:R7KFBuKIqeJANtRl@aws-0-us-west-2.pooler.supabase.com:6543/postgres"))
+        yield conn
+    finally:
+        conn.close()
