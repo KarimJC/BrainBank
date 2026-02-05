58e933d32cb2889fa86f8cee552af544e5f69893
Anderson Liew <alacc776@gmail.com>
2026-02-05 12:18:57 -0500

    fixing merge conflicts

--- backend/api/schemas/__init__.py
+++ backend/api/schemas/__init__.py
@@ -1,15 +1,5 @@
 from api.schemas.user import UserCreate, UserUpdate, UserResponse, DeleteResponse
-<<<<<<< HEAD
 from api.schemas.courses import CourseCreate, CourseUpdate, CourseResponse, CourseList, CourseDeleteResponse
-=======
-from api.schemas.courses import (
-    CourseCreate,
-    CourseUpdate,
-    CourseResponse,
-    CourseList,
-    CourseDeleteResponse
-)
->>>>>>> aa4dd7c0a6561dba03cc51751d6abc38db83a42f
 
 __all__ = [
     "UserCreate",
