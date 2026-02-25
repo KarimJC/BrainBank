f58593abefe4bfaabfe1e757d06952bfa76837b0
25abdullah <25abdullah.ismail@gmail.com>
2026-02-24 23:09:43 -0500

    update documentation for connection manager, work in progress routes for conversation

--- backend/api/websocket_manager/connection_manager.py
+++ backend/api/websocket_manager/connection_manager.py
@@ -1,6 +1,18 @@
 from typing import Dict
 from fastapi import WebSocket
 
+'''
+The purpose of this class ConnectionManager is to keep track of who is online and to make sure that messages are sent to the right person.
+Below is an example:
+
+user_id 55 connects, the connection will map their id of 55 to a websocket.
+user_id 56 connects, the connection will map their id of 56 to a websocket.
+user_id 56 sends a message to user_id 55, the send_message will look up id 55 
+and then send the text. The connection manager is purely for storage purposes only 
+and does not have any knowledge of what messages are being sent or who is sending them.
+'''
+
+
 class ConnectionManager:
     def __init__(self):
         #user_id maps to websocket 
