4dc8fa733ab10656dd44c1d61a1f488bba5448bf
Lauryn Gong <122813080+lauryngong@users.noreply.github.com>
2026-03-22 20:57:14 -0400

    Professor CRUD endpoints (#20)
    * commit
    
    * Apply suggestion from @AngelElMoucary
    
    Co-authored-by: AngelElMoucary <elmoucary.a@northeastern.edu>
    
    * fix professor crud backend endpoints
    
    * fixed lint errors
    
    * fixed exceptions
    
    * linter fix
    
    * update to professor routes
    
    * update to professor routes to fix errors
    
    * individ course page imp (#28)
    
    * fixing professor api tags
    
    * fix prefix
    
    ---------
    
    Co-authored-by: AngelElMoucary <elmoucary.a@northeastern.edu>

--- backend/api/routes/__init__.py
+++ backend/api/routes/__init__.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter
 from api.routes.user import router as user_router
 from api.routes.courses import router as courses_router
 from api.routes.message import router as message_router
-from api.routes.course_section import router as course_section_router 
+from api.routes.course_section import router as course_section_router
+from api.routes.professor import router as professor_router
 from api.routes.conversation import router as conversation_router
 from api.routes.notes import router as notes_router  
 from api.routes.ai_chat import router as ai_chat_router 
@@ -11,7 +12,7 @@ api_router = APIRouter()
 api_router.include_router(user_router, tags=["user"])
 api_router.include_router(courses_router, tags=["courses"])
 api_router.include_router(message_router, tags=["message"])
-api_router.include_router(course_section_router, tags=["course_sections"])
+api_router.include_router(professor_router, tags=["professors"])
 api_router.include_router(conversation_router, tags=["conversations"])
 
 api_router.include_router(course_section_router, tags=["course-sections"])
